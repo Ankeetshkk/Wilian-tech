a6a98e102f1763c0063cac5ad40b94b3558162f9
Ankit Shukla <ankeetshk@gmail.com>
2017-04-16 00:11:45 +0530

    Add fresh_tomatoes.py fresh_tomatoes.html movies.py weblayout.py media.py movies.py

---
+++ media.py
@@ -0,0 +1,7 @@
+class Video():
+    def __init__(self,title,video_storyline,video_img,trailer_youtube_url):
+        self.title=title;
+        self.video_storyline=video_storyline;
+        self.poster_image_url=video_img;
+        self.trailer_youtube_url=trailer_youtube_url;
+        
---
+++ movies.py
@@ -0,0 +1,13 @@
+import fresh_tomatoes;
+import media;
+
+video1=media.Video("Galaxy S8 and S8+ | Overview and thoughts+GIVEAWAY","","https://yt3.ggpht.com/-EdBe2d_iYh0/AAAAAAAAAAI/AAAAAAAAAAA/CKstnoJBfRY/s100-c-k-no-mo-rj-c0xffffff/photo.jpg%22","https://www.youtube.com/watch?v=h6r8pOWurP8&t=1s");
+video2=media.Video("top 5 android launchers you wanted | Less known edition","","https://yt3.ggpht.com/-EdBe2d_iYh0/AAAAAAAAAAI/AAAAAAAAAAA/CKstnoJBfRY/s100-c-k-no-mo-rj-c0xffffff/photo.jpg%22","https://www.youtube.com/watch?v=T4BYMJ7_f1I&t=199s");
+video3=media.Video("Dual/triple boot your PC with any LINUX distro | ( UBUNTU 16.04)  !","","https://yt3.ggpht.com/-EdBe2d_iYh0/AAAAAAAAAAI/AAAAAAAAAAA/CKstnoJBfRY/s100-c-k-no-mo-rj-c0xffffff/photo.jpg%22","https://www.youtube.com/watch?v=i2c3hBWCr2A&t=15s"); 
+video4=media.Video("How to make your windows 10/8 PC blazing fast in 2017","","https://yt3.ggpht.com/-EdBe2d_iYh0/AAAAAAAAAAI/AAAAAAAAAAA/CKstnoJBfRY/s100-c-k-no-mo-rj-c0xffffff/photo.jpg%22","https://www.youtube.com/watch?v=gI8h7fg5lMc&t=30s");  
+video5=media.Video("Top 5 android launchers of 2017 | The most stable edition +GIVEAWAY 100$","","https://yt3.ggpht.com/-EdBe2d_iYh0/AAAAAAAAAAI/AAAAAAAAAAA/CKstnoJBfRY/s100-c-k-no-mo-rj-c0xffffff/photo.jpg%22","https://www.youtube.com/watch?v=d0q90nzCyMk&t=67s");
+video6=media.Video("Play OVERWATCH,ASSASSINS CREED etc. on your ANDROID phone!!!- Top PC/console games on android/iOS","","https://yt3.ggpht.com/-EdBe2d_iYh0/AAAAAAAAAAI/AAAAAAAAAAA/CKstnoJBfRY/s100-c-k-no-mo-rj-c0xffffff/photo.jpg%22","https://www.youtube.com/watch?v=B0sNL8BR-aQ&t=1s");
+
+movies=[video1,video2,video3,video4,video5,video6];
+
+fresh_tomatoes.open_movies_page(movies);
